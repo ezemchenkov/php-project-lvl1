8f29fa7044b45d3796a2c05ebf867decdab95300
ezemchenkov <ezem4enkov@gmail.com>
2021-01-05 14:49:07 +0400

    game gcd

---
+++ src/Games/Gcd.php
@@ -0,0 +1,48 @@
+<?php
+declare(strict_types=1);
+
+namespace BrainGames\Games\Gcd;
+
+use function BrainGames\Engine\start;
+
+const MIN = 1;
+const MAX = 99;
+
+const DESCRIPTION = 'Find the greatest common divisor of given numbers.';
+
+function run(): void
+{
+    $game = function (): array {
+        $num1 = random_int(MIN, MAX);
+        $num2 = random_int(MIN, MAX);
+
+        $question = getQuestion($num1, $num2);
+        $correctAnswer = getAnswer($num1, $num2);
+        return [$question, $correctAnswer];
+    };
+
+    start($game, DESCRIPTION);
+}
+
+function getQuestion(int $num1, int $num2): string
+{
+    return "{$num1} {$num2}";
+}
+
+function getAnswer(int $num1, int $num2): string
+{
+    return (string) gcd($num1, $num2);
+}
+
+function gcd(int $num1, int $num2): int
+{
+    while ($num1 !== 0 && $num2 !== 0) {
+        if ($num1 > $num2) {
+            $num1 %= $num2;
+        } else {
+            $num2 %= $num1;
+        }
+    }
+
+    return $num1 + $num2;
+}

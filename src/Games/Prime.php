b1f357c5b89dd87af950d0ac03d36fe42e0e4a78
ezmchnkv <ezmchnkv@gmail.com>
2021-01-06 14:01:58 +0400

    game prime

---
+++ src/Games/Prime.php
@@ -0,0 +1,48 @@
+<?php
+
+declare(strict_types=1);
+
+namespace BrainGames\Games\Prime;
+
+use function BrainGames\Engine\start;
+
+const MIN = 1;
+const MAX = 999;
+
+const DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+function run(): void
+{
+    $game = function (): array {
+        $question = getQuestion();
+        $correctAnswer = getAnswer($question);
+        return [$question, $correctAnswer];
+    };
+
+    start($game, DESCRIPTION);
+}
+
+function getQuestion(): int
+{
+    return random_int(MIN, MAX);
+}
+
+function getAnswer(int $number): string
+{
+    return isPrime($number) ? 'yes' : 'no';
+}
+
+function isPrime(int $number): bool
+{
+    if ($number > 2 && $number % 2 === 0) {
+        return false;
+    }
+
+    for ($i = 3; $i < sqrt($number); $i += 2) {
+        if ($number % $i === 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
